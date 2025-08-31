51c600147dacd98310d3237b662548bb0505a880
cesar <cesar_mejia_99@hotmail.com>
2025-08-31 02:37:14 -0500

    arreglando error

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -4,20 +4,20 @@ export async function POST(req: Request) {
    const data = await req.json()
 
    if (data.name !== 'cesar') {
-      return NextResponse.json({ success: false }), { status: 400 }
+      return NextResponse.json({ success: false }, { status: 400 })
    }
 
    if (data.email !== 'cesar99dev@gmail.com') {
-      return NextResponse.json({ success: false }), { status: 400 }
+      return NextResponse.json({ success: false }, { status: 400 })
    }
 
    if (data.password !== '123456') {
-      return NextResponse.json({ success: false }), { status: 400 }
+      return NextResponse.json({ success: false }, { status: 400 })
    }
 
    // Simulación de validación / registro
    if (data.email && data.password && data.name) {
-      return NextResponse.json({ success: true })
+      return NextResponse.json({ success: true }, { status: 200 })
    }
 
    return NextResponse.json({ success: false }, { status: 400 })
