14580d75a72fd70140be234880ce4633e7a04d6a
cesar <cesar_mejia_99@hotmail.com>
2025-08-31 02:41:20 -0500

    arreglando otra vez

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 
-export async function POST(req: Request) {
+export async function POST(req: Request): Promise<Response> {
    const data = await req.json()
 
    if (data.name !== 'cesar') {
